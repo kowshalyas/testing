685fde198e25fbac5affd58341205a91d6f696de
kowshalyas <164656513+kowshalyas@users.noreply.github.com>
2024-04-05 00:05:07 +0530

    Update file2.sh

--- file2.sh
+++ file2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sum=`expr 10 + 20`
-sub=`expr 10 - 5`
-prod=`expr 10 \* 5`
-div=`expr 20 / 5`
-echo "sum of two numbers is $sum"
-echo "sub of two numbers is $sub"
-echo "prod of two numbers is $prod"
-echo "div of two numbers is $div"
+num2=$3
+product=1
+while [ $num1 -gt 0 ]
+do
+product=`expr $product \* $num1`
+num1=`expr $num1 -1
+done
+echo "the factorial of $num2 is $product"
 
