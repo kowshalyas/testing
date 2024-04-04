5d3b5985fc88237f9e97a072f90c88a0e29af70a
kowshalyas <164656513+kowshalyas@users.noreply.github.com>
2024-04-04 23:48:18 +0530

    Update file1.sh

--- file1.sh
+++ file1.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 sum=`expr 10 + 20`
+sub=`expr 10 - 5`
+prod=`expr 10 \* 5`
+div=`expr 20 / 5`
 echo "sum of two numbers is $sum"
+echo "sub of two numbers is $sub"
+echo "prod of two numbers is $prod"
+echo "div of two numbers is $div"
+
